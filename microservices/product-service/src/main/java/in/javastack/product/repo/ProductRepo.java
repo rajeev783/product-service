66734aec0e6c9c886d5617759f614bbb726f8971
Rajeev Kumar <rjvkumar783@gmail.com>
2025-04-10 08:18:03 +0530

    code update

--- microservices/product-service/src/main/java/in/javastack/product/repo/ProductRepo.java
+++ microservices/product-service/src/main/java/in/javastack/product/repo/ProductRepo.java
@@ -2,6 +2,8 @@ package in.javastack.product.repo;
 
 import in.javastack.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends MongoRepository<Product, String> {
 }
--- microservices/product-service/src/main/java/in/javastack/product/service/ProductService.java
+++ microservices/product-service/src/main/java/in/javastack/product/service/ProductService.java
@@ -1,11 +1,14 @@
 package in.javastack.product.service;
 
 import in.javastack.product.dto.ProductRequest;
+import in.javastack.product.dto.ProductResponse;
 import in.javastack.product.model.Product;
 import in.javastack.product.repo.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +24,13 @@ public class ProductService {
                 .build();
         productRepo.save(product);
         log.info("Product created successfully !");
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        return productRepo.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(),
+                        product.getDescription(), product.getPrice()))
+                .toList();
     }
 }
